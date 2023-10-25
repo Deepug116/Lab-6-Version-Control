903c968c3f8f9fa67ebad00e9c5de7f00424d7bb
Anna Folsom <annalisafolsom@gmail.com>
2023-10-25 15:16:22 -0400

    added decode() method

--- main.py
+++ main.py
@@ -7,6 +7,24 @@ def encode(password):
         encoded_password += str(digit)
     return encoded_password
 
+# Annalisa Folsom 10/25/2023
+# Decode() function
+def decode(encoded_password):
+    dec_list = list(encoded_password)  # Changes the str to a list.
+    index = 0
+    index_1 = 0
+    for element in dec_list:  # Changes str to int.
+        dec_list[index] = int(element)
+        index += 1
+    for element in range(len(dec_list)):  # Subtract 3 from each digit...if negative, add 10
+        dec_list[index_1] = dec_list[index_1] - 3
+        if dec_list[index_1] < 0:
+            dec_list[index_1] += 10
+        index_1 += 1
+    decoded_password = "".join(map(str, dec_list))  # This line structure is based on Deepak's commit to Annalisa's repo
+
+    return decoded_password
+
 if __name__ == '__main__':
     password = None
     encoded_password = None
