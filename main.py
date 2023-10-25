4b8008a61650ee447b08b7189837ff38f3a9dbec
Deepug116 <89586741+Deepug116@users.noreply.github.com>
2023-10-25 14:10:15 -0400

    added the encode function and the main method

--- main.py
+++ main.py
@@ -1,4 +1,28 @@
-# This is my first github addition
+# Deepak Guggilam 10/25/2023
+# Encode() function
 
-print("Hello World")
+def encode(password):
+    encoded_password = ''
+    for digit in password:
+        digit = (int(digit) + 3) % 10
+        encoded_password += str(digit)
+    return encoded_password
 
+if __name__ == '__main':
+    password = None
+    encoded_password = None
+    while True:
+        print("Menu")
+        print("-------------")
+        print("1. Encode")
+        print("2. Quit")
+        user_input = int(input("Please enter an option:"))
+
+        if user_input == 1:
+            password = input("Please enter your password to encode:")
+            encoded_password = encode(password)
+            print("Your password has been encoded and stored!\n")
+
+        elif user_input == 2:
+            # implement the decode function
+            break
